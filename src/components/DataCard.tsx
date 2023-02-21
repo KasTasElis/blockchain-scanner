538358df9c9e8289a55447fb0ecd96df00e99738
EliTamošauskas <tamosauskas09@gmail.com>
2023-02-21 14:37:44 +0200

    progress

--- src/components/DataCard.tsx
+++ src/components/DataCard.tsx
@@ -5,17 +5,17 @@ const DataCard = ({
 }: {
   title: string;
   subTitle: string;
-  data: { label: string; value: string }[];
+  data: { [label: string]: string | number };
 }) => (
   <div className="p-5 rounded mb-5 bg-slate-100 dark:bg-slate-800 dark:text-gray-200">
     <h3 className="font-semibold text-xl">{title}</h3>
     <h4 className="mb-5">{subTitle}</h4>
 
     <ul>
-      {data.map((item, index) => (
+      {Object.keys(data).map((key, index) => (
         <li key={index} className={data.length === index + 1 ? "mb-1" : ""}>
-          <span className="font-semibold">{`${item.label}: `}</span>
-          <span>{item.value}</span>
+          <span className="font-semibold">{`${key}: `}</span>
+          <span>{data[key]}</span>
         </li>
       ))}
     </ul>
--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,35 +1,114 @@
+import { useState } from "react";
 import { useSearchHistory } from "../utils";
+import { DataCard } from "../components";
+
+type TWalletInfo = {
+  Address: string;
+  "Total Received": number;
+  "Total Sent": number;
+  "Final Balance": number;
+  "No. of Transactions": number;
+};
 
 const SearchBox = () => {
   const { appendSearch } = useSearchHistory();
+  const [walletInfo, setWalletInfo] = useState<TWalletInfo | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [value, setValue] = useState("");
 
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const onChange = (e: any) => {
+    console.log(e.target.value);
+    setValue(e.target.value);
+  };
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const inputElement = document.querySelector(
-      "#searchInput"
-    ) as HTMLInputElement | null;
-    if (!inputElement) {
-      throw Error("Wtf is going on, there is no search input element...?");
-    }
+    const URL = "https://blockchain.info/rawaddr";
+    const QueryURL = `${URL}/${value}`;
+    const res = await fetch(QueryURL);
+    console.log({ res });
+    const data = await res.json();
+    console.log({ data });
 
-    appendSearch(inputElement.value);
+    // to historical search record
+    appendSearch(value);
+
+    // data.XYZ could be any data.. how would i annotate it here?
+    const newWalletInfo = {
+      Address: data.address,
+      "Total Received": data.total_received,
+      "Total Sent": data.total_sent,
+      "Final Balance": data.final_balance,
+      "No. of Transactions": data.n_tx,
+    };
+
+    setWalletInfo(newWalletInfo);
   };
+
+  const handlePaste = async () => {
+    const pasteResult = await navigator.clipboard.readText();
+    setValue(pasteResult);
+  };
+
+  // useEffect(() => {
+  //   // show custom html5 validation message
+  //   const inputEl = document.querySelector(
+  //     "#searchInput"
+  //   ) as HTMLInputElement | null;
+  //   if (!inputEl) return;
+
+  //   inputEl.setCustomValidity(
+  //     "Please enter a valid BTC wallet address or transaction hash."
+  //   );
+  // }, []);
 
   return (
     <div className="py-5 rounded">
       <form onSubmit={onSubmit} className="flex space-x-4">
-        <input
-          id="searchInput"
-          type="text"
-          placeholder="TX Hash / Wallet Address"
-          className="bg-slate-100 p-3 rounded w-full focus-visible:outline-amber-500 text-md"
-          required
-        />
+        <div className="w-full relative">
+          <input
+            id="searchInput"
+            type="text"
+            placeholder="TX Hash / Wallet Address"
+            className="bg-slate-100 p-3 rounded w-full focus-visible:outline-amber-500 text-md h-full"
+            required
+            //pattern="/^([13]{1}[a-km-zA-HJ-NP-Z1-9]{26,33}|bc1[a-z0-9]{39,62})$"
+            maxLength={90}
+            minLength={11}
+            onChange={onChange}
+            value={value}
+          />
+          <div className="absolute right-0 top-0 h-full pr-5 flex justify-center items-center">
+            <button
+              type="button"
+              className="text-sm cursor-pointer text-gray-500 hover:opacity-70"
+              onClick={handlePaste}
+            >
+              PASTE
+            </button>
+          </div>
+        </div>
         <button className="px-5 py-3 bg-amber-500 rounded hover:bg-amber-400 font-semibold text-md md:text-2xl">
           🔍
         </button>
       </form>
+
+      {loading ? (
+        <h3 className="text-center text-lg font-semibold p-5">Loading...</h3>
+      ) : !walletInfo ? (
+        <h3 className="text-center text-lg font-semibold p-5">
+          Nothing to show...
+        </h3>
+      ) : (
+        <div className="mt-5">
+          <DataCard
+            title="💰 Wallet Address"
+            subTitle={walletInfo.Address}
+            data={{ ...walletInfo }}
+          />
+        </div>
+      )}
     </div>
   );
 };
